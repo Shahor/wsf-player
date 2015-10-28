13db1840554e0703b5a099e5d45c4a40aedc6c31
Shahor <shahor@shahor.fr>
2015-10-28 02:24:31 +0100

    Preparing next lesson

---
+++ scripts/helpers.js
@@ -0,0 +1,31 @@
+(function () {
+	'use strict';
+
+	var cb = function () {};
+
+	function doXHR(url, callback) {
+		/* Pooor poooor pooooooor man's XHR
+		I'm doing this because current browsers don't allow
+		for XHR calls easily on localhost (without a server answering
+		the proper headers) because of CORS policies.
+		I know this is ugly, and right now I don't care, I just need
+		it to work for the lesson's purpose
+		*/
+
+		var script = document.createElement('script');
+		script.setAttribute('src', url);
+
+		document.body.appendChild(script);
+
+		script.addEventListener('load', function (e) {
+			script.remove();
+		})
+		cb = callback;
+	}
+
+	window.jsonpCallback = function (data) {
+		cb(data);
+	}
+
+	window.doXHR = doXHR;
+}());
